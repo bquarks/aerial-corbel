d08133499a58eb3d04f5b53115b48811da492b7c
DANIEL GARCÍA <daniel.garcia@bq.com>
2016-05-09 15:59:42 +0200

    Release 0.2.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'bquarks:aerial-corbel',
-  version: '0.2.0',
+  version: '0.2.1',
 
   // Brief, one-line summary of the package.
   summary: 'Low level corbel api handler and tranlator',
