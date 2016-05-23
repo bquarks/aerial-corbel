9b468ed59f996c39b50ab8814565cd56e3da40f5
javiergarciagonzalez <javier.garciagonzalez@bq.com>
2016-05-23 11:20:22 +0200

    Fix updating collection or resource

--- corbel-handler.js
+++ corbel-handler.js
@@ -7,13 +7,17 @@ var Corbel = Npm.require('corbel-js'),
 
 CorbelHandler = {
 
-  request (corbelDriver, relation, query) {
+  getRequest (corbelDriver, relation, query, domain) {
     // TODO: avoid repeat the first methods
+
+
+    let curDomain = domain || corbelDriver.config.config.domain;
+
     if (relation.method === 'relation') {
-      return corbelDriver.domain(corbelDriver.config.domain).resources[relation.method](relation.from, relation.field, relation.to).get(null, query);
+      return corbelDriver.domain(curDomain).resources[relation.method](relation.from, relation.field, relation.to).get(null, query);
     }
     else {
-      return corbelDriver.domain(corbelDriver.config.domain).resources[relation.method](relation.name).get(query);
+      return corbelDriver.domain(curDomain).resources[relation.method](relation.name).get(query);
     }
   },
 
@@ -28,7 +32,7 @@ CorbelHandler = {
       _.extend(query, QueryTranslator.options(getParams.options), distinct);
 
       CorbelHandler
-      .request(corbelDriver, relation, query)
+      .getRequest(corbelDriver, relation, query)
       .then((res) => {
         if (res && res.data) {
           future.return(res.data);
@@ -60,15 +64,12 @@ CorbelHandler = {
 
     if (collectionName && getParams) {
       let future = new Future,
-          query = QueryTranslator.query(getParams.selector),
-          relation = QueryTranslator.relation(collectionName, getParams.options),
-          distinct = QueryTranslator.distinct(getParams.distinct);
+          options = getParams.options,
+          mod = getParams.mod,
+          selector = getParams.selector;
 
-      _.extend(query, QueryTranslator.options(getParams.options), distinct);
-
-      corbelDriver.resources.collection(collectionName)
-      .update(getParams.mod,
-       query)
+      CorbelHandler
+      .updateRequest(corbelDriver, collectionName, selector._id, mod, options)
       .then((res) => {
         if (res) {
           console.log('Updated!');
@@ -89,6 +90,17 @@ CorbelHandler = {
       if (!getParams) {
         throw new TypeError('The third parameter must be a valid get params object');
       }
+    }
+  },
+
+  updateRequest(corbelDriver, colName, id, mod, options) {
+    let curDomain = corbelDriver.config.config.domain;
+
+    if (options.multi) { //Update all documents in a collection
+      return corbelDriver.domain(curDomain).resources.collection(colName).update(mod, selector);
+    }
+    else {
+      return corbelDriver.domain(curDomain).resources.resource(colName, id).update(mod);
     }
   },
 
