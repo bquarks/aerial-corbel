ebaf78dfa397afbd7cf15e7ff8bd778a36213b64
Francisco López <donflopez@gmail.com>
2016-07-15 11:33:00 +0200

    Update version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'bquarks:aerial-corbel',
-    version: '0.3.0',
+    version: '0.3.1',
 
     // Brief, one-line summary of the package.
     summary: 'Low level corbel api handler and tranlator',
